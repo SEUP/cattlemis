3b8a6a74b10f172e5897e08c266591fd35321226
Chaow Porkaew <chaow.po@up.ac.th>
2016-06-04 12:27:53 +0700

    update layout

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta id="token" name="token" content="{{ csrf_token() }}">
 
-    <title>Chaow Porkaew</title>
+    <title>Cattle MIS</title>
 
     <link rel="stylesheet" href="/css/vendor.sass.css">
     <link rel="stylesheet" href="/css/vendor.less.css">
@@ -25,7 +25,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="/admin">Chaow Porkaew's Management Information System</a>
+            <a class="navbar-brand" href="/admin">Cattle Management Information System</a>
         </div>
         <!-- /.navbar-header -->
 
@@ -261,28 +261,32 @@
                     <li>
                         <a href="/admin#"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
                     </li>
+
                     <li>
-                        <a href="/admin/calendar"><i class="fa fa-calendar fa-fw"></i> Calendar</a>
+                        <a href="/admin/questionair"><i class="fa fa-plus fa-fw"></i> บันทึกข้อมูลเกษตรกรใหม่</a>
+                    </li>
+
+                    <li>
+                        <a href="/admin/questionair-search"><i class="fa fa-search fa-fw"></i> ค้นหาข้อมูลเกษตรกร</a>
+                    </li>
+                    <li>
+                        <a href="/admin/#chart"><i class="fa fa-bar-chart-o fa-fw"></i> แผนภูมิรายงาน<span class="fa arrow"></span></a>
+                        <ul class="nav nav-second-level">
+                            <li>
+                                <a href="flot.html">จำนวนเกษตรกรแยกตามจังหวัด</a>
+                            </li>
+                            <li>
+                                <a href="morris.html">จำนวนเกษตรกรแบ่งตามรายได้</a>
+                            </li>
+                        </ul>
+                        <!-- /.nav-second-level -->
                     </li>
                     <li>
                         <a href="/admin/user"><i class="fa fa-user fa-fw"></i> User</a>
                     </li>
 
-                    <li>
-                        <a href="/admin/course"><i class="fa fa-television fa-fw"></i> Course</a>
-                    </li>
-                    <li>
-                        <a href="#"><i class="fa fa-bar-chart-o fa-fw"></i> Charts<span class="fa arrow"></span></a>
-                        <ul class="nav nav-second-level">
-                            <li>
-                                <a href="flot.html">Flot Charts</a>
-                            </li>
-                            <li>
-                                <a href="morris.html">Morris.js Charts</a>
-                            </li>
-                        </ul>
-                        <!-- /.nav-second-level -->
-                    </li>
+
+
                     <li>
                         <a href="tables.html"><i class="fa fa-table fa-fw"></i> Tables</a>
                     </li>
--- resources/views/admin/user/profile.blade.php
+++ resources/views/admin/user/profile.blade.php
@@ -60,40 +60,6 @@
         </div>
     </div>
 
-    <div class="row" id="user-social-app">
-        <div class="col-lg-12">
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    Facebook
-                </div>
-                <div class="panel-body">
-                    @if(Auth::user()->facebook)
-                        <dl class="dl-horizontal">
-                            <dt>Facebook ID</dt>
-                            <dd>{{ Auth::user()->facebook->facebook_id }}</dd>
-                            <dt>Facebook E-Mail</dt>
-                            <dd>{{ Auth::user()->facebook->email }}</dd>
-                            <dt>Facebook Name</dt>
-                            <dd>{{ Auth::user()->facebook->name }}</dd>
-                        </dl>
-                        <a href="{{action("Admin\\ProfileController@doUnlink",['social'=>'facebook'])}}"
-                           class="btn btn-social btn-facebook">
-                            <span class="fa fa-facebook"></span> Unlink with Facebook
-                        </a>
-                    @else
-                        <?php
-                        $fb = app(SammyK\LaravelFacebookSdk\LaravelFacebookSdk::class);
-                        $callbackUrl = action("Admin\\ProfileController@socialCallback", ['social' => 'facebook']);
-                        $url = $fb->getLoginUrl(['email'], $callbackUrl);
-                        ?>
-                        <a href="{{$url}}" class="btn btn-social btn-facebook">
-                            <span class="fa fa-facebook"></span> Link with Facebook
-                        </a>
-                    @endif
-                </div>
-            </div>
-        </div>
-    </div>
 
 @endsection
 
