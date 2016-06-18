3bbbf77f40fb324e76d98b6a1f792a6fa1f659b4
chontipan <chontipan@gmail.com>
2016-06-18 22:26:45 +0700

    create farm owner jobs tbl

---
+++ database/migrations/2016_06_18_152225_create_farm_owner_jobs_tbl.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFarmOwnerJobsTbl extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('farm_owners_jobs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('jobtype');
+            $table->string('tumsuantype')->nullable();
+            $table->string('tumraitype')->nullable();
+            $table->string('other')->nullable();
+            //$table->integer('house_no');
+            $table->unsignedInteger('farm_owner_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('farm_owners_jobs');
+    }
+}
