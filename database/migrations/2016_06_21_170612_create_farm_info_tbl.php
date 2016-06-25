7d6efba7afd21f34e48f2048bbea62e15928bcc5
chontipan <chontipan@gmail.com>
2016-06-25 15:21:49 +0700

    add part2 round2

--- database/migrations/2016_06_21_170612_create_farm_info_tbl.php
+++ database/migrations/2016_06_21_170612_create_farm_info_tbl.php
@@ -12,13 +12,9 @@ class CreateFarmInfoTbl extends Migration
      */
     public function up()
     {
-
-        Schema::create('choice_farm_info', function (Blueprint $table) {
+        Schema::create('farm_info', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('choice_id');
             $table->integer('farm_owner_id');
-            $table->string('remark')->nullable();
-            $table->integer('total_expense_amount')->nullable();
             $table->integer('total_cattle_amount')->nullable();
             $table->integer('total_male_breeder_amount')->nullable();
             $table->integer('total_female_breeder_amount')->nullable();
@@ -26,14 +22,27 @@ class CreateFarmInfoTbl extends Migration
             $table->integer('total_female_cattle_aged_over_six_amount')->nullable();
             $table->integer('total_male_cattle_aged_under_six_amount')->nullable();
             $table->integer('total_female_cattle_aged_under_six_amount')->nullable();
-            $table->string('cattle_source')->nullable();
-            $table->integer('cattle_amount')->nullable();
-            $table->integer('cattle_price')->nullable();
+            $table->integer('total_expense_amount')->nullable();
+            $table->integer('drug_price')->nullable();
+            $table->integer('food_price')->nullable();
+            $table->string('expense_remark')->nullable();
+            $table->integer('expense_price')->nullable();
+            $table->timestamps();
+
+        });
+        
+        Schema::create('choice_farm_info', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('choice_id');
+            $table->integer('farm_owner_id');
+            $table->string('remark')->nullable();
+            $table->integer('amount')->nullable();
+            $table->string('source')->nullable();
+            $table->integer('price')->nullable();
             $table->timestamps();
 
         });
     }
-
     /**
      * Reverse the migrations.
      *
@@ -41,6 +50,7 @@ class CreateFarmInfoTbl extends Migration
      */
     public function down()
     {
+        Schema::dropIfExists('farm_info');
         Schema::dropIfExists('choice_farm_info');
     }
 }
