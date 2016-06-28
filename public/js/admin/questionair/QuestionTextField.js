457a2e3cfb85a634e72f3cca39090f73688429c7
Chaow Porkaew <chaow.po@up.ac.th>
2016-06-28 13:52:23 +0700

    fix tyop

--- public/js/admin/questionair/QuestionTextField.js
+++ public/js/admin/questionair/QuestionTextField.js
@@ -7,7 +7,7 @@ var QuestionTextField = Vue.extend({
     <div class="form-group">
             <label class="col-sm-2 control-label">{{label}}</label>
             <div class="col-sm-10">
-                <input type="text" v-model="model" class="form-control" placeholder="{{placeHolder}}"/>
+                <input type="text" v-model="model" class="form-control" placeholder="{{placeholder}}"/>
             </div>
         </div>
     `,
@@ -19,7 +19,7 @@ var QuestionTextField = Vue.extend({
             required: true,
             twoWay: true
         },
-        placeHolder: {}
+        placeholder: {}
     }
 });
 
