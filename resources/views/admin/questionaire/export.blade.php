3954075dc9351af15d6ee2c2f45263a047c991d4
Chaow Porkaew <chaow.po@up.ac.th>
2016-08-01 07:40:50 +0700

    add thai font

--- resources/views/admin/questionaire/export.blade.php
+++ resources/views/admin/questionaire/export.blade.php
@@ -1,8 +1,41 @@
 <html>
 <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <title></title>
+
+    <style>
+
+        @font-face {
+            font-family: "TH Niramit AS";
+            src: url('{{public_path()}}/fonts/thai/TH Niramit AS.ttf');
+        }
+        @font-face {
+            font-family: "TH Niramit AS";
+            src: url('{{public_path()}}/fonts/thai/TH Niramit AS Bold.ttf');
+            font-weight: bold;
+        }
+        @font-face {
+            font-family: "TH Niramit AS";
+            src: url('{{public_path()}}/fonts/thai/TH Niramit AS Italic.ttf');
+            font-style: italic,oblique;
+        }
+        @font-face {
+            font-family: "DejaVu Sans";
+            src: url('{{public_path()}}/fonts/thai/TH Niramit AS Bold Italic.ttf');
+            font-weight: bold;
+            font-style: italic,oblique;
+        }
+
+        body, h1, h2, h3, h4, h5, h6 {
+            font-family: 'TH Niramit AS';
+        }
+
+
+    </style>
+
 </head>
 <body>
-<h1>Hello Test</h1>
+รายงาน
+<h1>รายงาน</h1>
 </body>
 </html>
