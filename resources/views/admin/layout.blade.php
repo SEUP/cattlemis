879260b936ddb3fa3b4591b98514e0cde3db1e49
chontipan <chontipan@gmail.com>
2016-08-17 18:45:34 +0700

    remove search TAB

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -65,14 +65,7 @@
 
                     <li class="sidebar-search">
 
-                        <div class="input-group custom-search-form">
-                            <input type="text" class="form-control" placeholder="Search...">
-                            <span class="input-group-btn">
-                                <button class="btn btn-default" type="button">
-                                    <span class="fa fa-search"></span>
-                                </button>
-                            </span>
-                        </div>
+                       
                         <!-- /input-group -->
                     </li>
                     <li class="sidebar-search">
