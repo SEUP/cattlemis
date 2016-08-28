c82a0c72c4b98530837f5bb79bcb5862c6352ffe
Chaow Porkaew <chaow.po@up.ac.th>
2016-08-28 08:26:09 +0700

    remove menu settings

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -50,8 +50,6 @@
                 <ul class="dropdown-menu dropdown-user">
                     <li><a href="/admin/profile"><i class="fa fa-user fa-fw"></i> User Profile</a>
                     </li>
-                    <li><a href="#"><i class="fa fa-gear fa-fw"></i> Settings</a>
-                    </li>
                     <li class="divider"></li>
                     <li><a href="/logout"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
                     </li>
