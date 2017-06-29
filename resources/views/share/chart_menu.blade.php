7a41604c12caa29f95424f3621376dda1f543c93
Chaow Porkaew <pchaowmobile@gmail.com>
2017-06-29 15:02:00 +0700

    fix path

--- resources/views/share/chart_menu.blade.php
+++ resources/views/share/chart_menu.blade.php
@@ -8,19 +8,19 @@
         <div id="collapse1" class="panel-collapse collapse in">
             <div class="panel-body">
                 <li><a href="{{url($PREFIX."/charts/pie/เพศ/sex")}}">เพศ</a></li>
-                <li><a href="{{$PREFIX}}/charts/range-farmowner/อายุ/age/15/70/8">อายุ</a></li>
-                <li><a href="{{$PREFIX}}/charts/normal/การศึกษา/education">การศึกษา</a></li>
-                <li><a href="{{$PREFIX}}/charts/gmap/ที่ตั้งฟาร์มของเกษตรกร">ที่ตั้งฟาร์มของเกษตรกร</a></li>
-                <li><a href="{{$PREFIX}}/charts/pie/อาชีพการเลี้ยงโคเนื้อ(อาชีพหลัก-เสริม)/cattle_job">อาชีพการเลี้ยงโคเนื้อ(อาชีพหลัก-เสริม)</a>
+                <li><a href="{{url($PREFIX."/charts/range-farmowner/อายุ/age/15/70/8")}}">อายุ</a></li>
+                <li><a href="{{url($PREFIX."/charts/normal/การศึกษา/education")}}">การศึกษา</a></li>
+                <li><a href="{{url($PREFIX."/charts/gmap/ที่ตั้งฟาร์มของเกษตรกร")}}">ที่ตั้งฟาร์มของเกษตรกร</a></li>
+                <li><a href="{{url($PREFIX."/charts/pie/อาชีพการเลี้ยงโคเนื้อ(อาชีพหลัก-เสริม)/cattle_job")}}">อาชีพการเลี้ยงโคเนื้อ(อาชีพหลัก-เสริม)</a>
                 </li>
                 <li>
-                    <a href="{{$PREFIX}}/charts/normal/เฉลี่ยรายได้รวมของครอบครัว/income_range">เฉลี่ยรายได้รวมของครอบครัว</a>
+                    <a href="{{url($PREFIX."/charts/normal/เฉลี่ยรายได้รวมของครอบครัว/income_range")}}">เฉลี่ยรายได้รวมของครอบครัว</a>
                 </li>
                 <li>
-                    <a href="{{$PREFIX}}/charts/normal/รายได้จากการประกอบอาชีพของเกษตรกร/jobtypes">รายได้จากการประกอบอาชีพของเกษตรกร</a>
+                    <a href="{{url($PREFIX."/charts/normal/รายได้จากการประกอบอาชีพของเกษตรกร/jobtypes")}}">รายได้จากการประกอบอาชีพของเกษตรกร</a>
                 </li>
                 <li>
-                    <a href="{{$PREFIX}}/charts/range-farmowner/รายได้เฉลี่ยต่อปีของการขายโคเนื้อ/avg_cattle_income/50000/200000/3?withNull=true&nullText=ยังไม่ได้ขาย">รายได้เฉลี่ยต่อปีของการขายโคเนื้อ</a>
+                    <a href="{{url($PREFIX."/charts/range-farmowner/รายได้เฉลี่ยต่อปีของการขายโคเนื้อ/avg_cattle_income/50000/200000/3?withNull=true&nullText=ยังไม่ได้ขาย")}}">รายได้เฉลี่ยต่อปีของการขายโคเนื้อ</a>
                 </li>
             </div>
         </div>
@@ -34,28 +34,28 @@
         </div>
         <div id="collapse2" class="panel-collapse collapse in">
             <div class="panel-body">
-                <li><a href="{{$PREFIX}}/charts/normal/วัตถุประสงค์ของการเลี้ยงโคเนื้อ/farm_purposes">วัตถุประสงค์ของการเลี้ยงโคเนื้อ</a>
+                <li><a href="{{url($PREFIX."/charts/normal/วัตถุประสงค์ของการเลี้ยงโคเนื้อ/farm_purposes")}}">วัตถุประสงค์ของการเลี้ยงโคเนื้อ</a>
                 </li>
-                <li><a href="{{$PREFIX}}/charts/pie/การทำประวัติโคเนื้อ/farm_record">การทำประวัติโคเนื้อ</a></li>
-                <li><a href="{{$PREFIX}}/charts/cattle/">จำนวนโคเนื้อที่เลี้ยง</a></li>
+                <li><a href="{{url($PREFIX."/charts/pie/การทำประวัติโคเนื้อ/farm_record")}}">การทำประวัติโคเนื้อ</a></li>
+                <li><a href="{{url($PREFIX."/charts/cattle/")}}">จำนวนโคเนื้อที่เลี้ยง</a></li>
                 <li>
-                    <a href="{{$PREFIX}}/charts/normal/ประสบการณ์การเลี้ยงโคเนื้อ/farm_exp">ประสบการณ์การเลี้ยงโคเนื้อ</a>
-                </li>
-                <li>
-                    <a href="{{$PREFIX}}/charts/normal/เป้าหมายการเลี้ยงในอนาคต/farm_future">เป้าหมายการเลี้ยงในอนาคต</a>
-                </li>
-                <li><a href="{{$PREFIX}}/charts/double/การขึ้นทะเบียนฟาร์มกับภาครัฐ/farm_register_status">การขึ้นทะเบียนฟาร์มกับภาครัฐ</a>
+                    <a href="{{url($PREFIX."/charts/normal/ประสบการณ์การเลี้ยงโคเนื้อ/farm_exp")}}">ประสบการณ์การเลี้ยงโคเนื้อ</a>
                 </li>
                 <li>
-                    <a href="{{$PREFIX}}/charts/multi-choice/การเกิดโรคในฟาร์ม/abortion,tuberculosis,foot_mouth_disease,disease_other">การเกิดโรคในฟาร์ม</a>
+                    <a href="{{url($PREFIX."/charts/normal/เป้าหมายการเลี้ยงในอนาคต/farm_future")}}">เป้าหมายการเลี้ยงในอนาคต</a>
+                </li>
+                <li><a href="{{url($PREFIX."/charts/double/การขึ้นทะเบียนฟาร์มกับภาครัฐ/farm_register_status")}}">การขึ้นทะเบียนฟาร์มกับภาครัฐ</a>
+                </li>
+                <li>
+                    <a href="{{url($PREFIX."/charts/multi-choice/การเกิดโรคในฟาร์ม/abortion,tuberculosis,foot_mouth_disease,disease_other")}}">การเกิดโรคในฟาร์ม</a>
                 </li>
 
                 <li>
-                    <a href="{{$PREFIX}}/charts/range-farmowner/ค่าใช้จ่ายในการเลี้ยงโคเนื้อ (บาท ต่อ เดือน)/total_expense_amount/1000/10000/3?withNull=true&nullText=ยังไม่ได้ขาย">ค่าใช้จ่ายในการเลี้ยงโคเนื้อ</a>
+                    <a href="{{url($PREFIX."/charts/range-farmowner/ค่าใช้จ่ายในการเลี้ยงโคเนื้อ (บาท ต่อ เดือน)/total_expense_amount/1000/10000/3?withNull=true&nullText=ยังไม่ได้ขาย")}}">ค่าใช้จ่ายในการเลี้ยงโคเนื้อ</a>
                 </li>
                 <li>
-                    <a href="{{$PREFIX}}/charts/normal/ประวัติการตรวจโรคสัตว์/farm_disease_check">ประวัติการตรวจโรคสัตว์</a>
-                <li><a href="{{$PREFIX}}/charts/normal/แหล่งน้ำที่ใช้/water_source_types">แหล่งน้ำที่ใช้</a>
+                    <a href="{{url($PREFIX."/charts/normal/ประวัติการตรวจโรคสัตว์/farm_disease_check")}}">ประวัติการตรวจโรคสัตว์</a>
+                <li><a href="{{url($PREFIX."/charts/normal/แหล่งน้ำที่ใช้/water_source_types")}}">แหล่งน้ำที่ใช้</a>
 
 
             </div>
@@ -71,27 +71,27 @@
         </div>
         <div id="collapse3" class="panel-collapse collapse in">
             <div class="panel-body">
-                <li><a href="{{$PREFIX}}/charts/range-farmowner/จำนวนแรงงานภายในครอบครัว/family_workers_amount/1/5/4">จำนวนแรงงานภายในครอบครัว</a>
+                <li><a href="{{url($PREFIX."/charts/range-farmowner/จำนวนแรงงานภายในครอบครัว/family_workers_amount/1/5/4")}}">จำนวนแรงงานภายในครอบครัว</a>
                 </li>
-                <li><a href="{{$PREFIX}}/charts/range-farmowner/จำนวนแรงงานภายนอก/external_workers_amount/1/5/4">จำนวนแรงงานภายนอก</a>
-                </li>
-                <li>
-                    <a href="{{$PREFIX}}/charts/pie/ลักษณะการเลี้ยงโคเนื้อ/take_care_types">ลักษณะการเลี้ยงโคเนื้อ</a>
+                <li><a href="{{url($PREFIX."/charts/range-farmowner/จำนวนแรงงานภายนอก/external_workers_amount/1/5/4")}}">จำนวนแรงงานภายนอก</a>
                 </li>
                 <li>
-                    <a href="{{$PREFIX}}/charts/double/พื้นที่ที่ใช้ในการเลี้ยงโคเนื้อ/use_land/">พื้นที่ที่ใช้ในการเลี้ยงโคเนื้อ</a>
+                    <a href="{{url($PREFIX."/charts/pie/ลักษณะการเลี้ยงโคเนื้อ/take_care_types")}}">ลักษณะการเลี้ยงโคเนื้อ</a>
                 </li>
-                <li><a href="{{$PREFIX}}/charts/pie/อาหารที่ใช้เลี้ยงโคเนื้อ/feed_types">อาหารที่ใช้เลี้ยงโคเนื้อ</a>
+                <li>
+                    <a href="{{url($PREFIX."/charts/double/พื้นที่ที่ใช้ในการเลี้ยงโคเนื้อ/use_land/")}}">พื้นที่ที่ใช้ในการเลี้ยงโคเนื้อ</a>
                 </li>
-                <li><a href="{{$PREFIX}}/charts/pie/การให้แร่ธาตุก้อน/minerals_feed">การให้แร่ธาตุก้อน</a></li>
-                <li><a href="{{$PREFIX}}/charts/double/การสำรองฟางข้าว หรือเปลือกข้าวโพด/feedstock">การสำรองฟางข้าว
+                <li><a href="{{url($PREFIX."/charts/pie/อาหารที่ใช้เลี้ยงโคเนื้อ/feed_types")}}">อาหารที่ใช้เลี้ยงโคเนื้อ</a>
+                </li>
+                <li><a href="{{url($PREFIX."/charts/pie/การให้แร่ธาตุก้อน/minerals_feed")}}">การให้แร่ธาตุก้อน</a></li>
+                <li><a href="{{url($PREFIX."/charts/double/การสำรองฟางข้าว หรือเปลือกข้าวโพด/feedstock")}}">การสำรองฟางข้าว
                         หรือเปลือกข้าวโพด</a></li>
                 <li>
-                    <a href="{{$PREFIX}}/charts/normal/แหล่งอาหารหยาบในฤดูแล้ง/feed_summer_sources">แหล่งอาหารหยาบในฤดูแล้ง</a>
+                    <a href="{{url($PREFIX."/charts/normal/แหล่งอาหารหยาบในฤดูแล้ง/feed_summer_sources")}}">แหล่งอาหารหยาบในฤดูแล้ง</a>
                 </li>
 
-                <li><a href="{{$PREFIX}}/charts/range-farmowner/พื้นที่ถือครองทางการเกษตร/total_own_lands/0/50/4">พื้นที่ถือครองทางการเกษตร</a>
-                <li><a href="{{$PREFIX}}/charts/normal/การเช่าที่ดินเพื่อใช้ในการเลี้ยง/rent_land">การเช่าที่ดินเพื่อใช้ในการเลี้ยง</a>
+                <li><a href="{{url($PREFIX."/charts/range-farmowner/พื้นที่ถือครองทางการเกษตร/total_own_lands/0/50/4")}}">พื้นที่ถือครองทางการเกษตร</a>
+                <li><a href="{{url($PREFIX."/charts/normal/การเช่าที่ดินเพื่อใช้ในการเลี้ยง/rent_land")}}">การเช่าที่ดินเพื่อใช้ในการเลี้ยง</a>
 
 
             </div>
@@ -108,27 +108,27 @@
         <div id="collapse4" class="panel-collapse collapse in">
             <div class="panel-body">
                 <li>
-                    <a href="{{$PREFIX}}/charts/pie/การผสมพันธุ์โคเนื้อในฟาร์ม/farm_breeding_type">การผสมพันธุ์โคเนื้อในฟาร์ม</a>
+                    <a href="{{url($PREFIX."/charts/pie/การผสมพันธุ์โคเนื้อในฟาร์ม/farm_breeding_type")}}">การผสมพันธุ์โคเนื้อในฟาร์ม</a>
                 </li>
                 <li>
-                    <a href="{{$PREFIX}}/charts/normal/แหล่งน้ำเชื้อการผสมเทียม/inseminate_sources">แหล่งน้ำเชื้อการผสมเทียม</a>
+                    <a href="{{url($PREFIX."/charts/normal/แหล่งน้ำเชื้อการผสมเทียม/inseminate_sources")}}">แหล่งน้ำเชื้อการผสมเทียม</a>
                 </li>
-                <li><a href="{{$PREFIX}}/charts/normal/ผู้ให้บริการผสมเทียม/breeders">ผู้ให้บริการผสมเทียม</a></li>
-                <li><a href="{{$PREFIX}}/charts/normal/การตายของโคในรอบปี/cattle_death">การตายของโคในรอบปี</a></li>
-                <li><a href="{{$PREFIX}}/charts/normal/บุคคลที่ทำการรักษาให้เมื่อโคเจ็บป่วย/disease_cured_by">บุคคลที่ทำการรักษาให้เมื่อโคเจ็บป่วย</a>
+                <li><a href="{{url($PREFIX."/charts/normal/ผู้ให้บริการผสมเทียม/breeders")}}">ผู้ให้บริการผสมเทียม</a></li>
+                <li><a href="{{url($PREFIX."/charts/normal/การตายของโคในรอบปี/cattle_death")}}">การตายของโคในรอบปี</a></li>
+                <li><a href="{{url($PREFIX."/charts/normal/บุคคลที่ทำการรักษาให้เมื่อโคเจ็บป่วย/disease_cured_by")}}">บุคคลที่ทำการรักษาให้เมื่อโคเจ็บป่วย</a>
                 </li>
 
-                <li><a href="{{$PREFIX}}/charts/range-farmowner/อัตราส่วนพ่อพันธุ์คุมฝูง/breeding_rate/1/20/3">อัตราส่วนพ่อพันธุ์คุมฝูง</a>
+                <li><a href="{{url($PREFIX."/charts/range-farmowner/อัตราส่วนพ่อพันธุ์คุมฝูง/breeding_rate/1/20/3")}}">อัตราส่วนพ่อพันธุ์คุมฝูง</a>
                 </li>
 
                 <li>
-                    <a href="{{$PREFIX}}/charts/doublePivot/การถ่ายพยาธิ/dewormed_amount/">การถ่ายพยาธิ</a>
+                    <a href="{{url($PREFIX."/charts/doublePivot/การถ่ายพยาธิ/dewormed_amount/")}}">การถ่ายพยาธิ</a>
                 </li>
 
 
-                <li><a href="{{$PREFIX}}/charts/double/วัคซีนป้องกันโรค/vaccine_ever">วัคซีนป้องกันโรค</a>
+                <li><a href="{{url($PREFIX."/charts/double/วัคซีนป้องกันโรค/vaccine_ever")}}">วัคซีนป้องกันโรค</a>
                 </li>
-                <li><a href="{{$PREFIX}}/charts/normal/การจัดการมูลโคในฟาร์ม/cattle_dung_uses">การจัดการมูลโคในฟาร์ม</a>
+                <li><a href="{{url($PREFIX."/charts/normal/การจัดการมูลโคในฟาร์ม/cattle_dung_uses")}}">การจัดการมูลโคในฟาร์ม</a>
                 </li>
             </div>
         </div>
@@ -142,7 +142,7 @@
         </div>
         <div id="collapse5" class="panel-collapse collapse in">
             <div class="panel-body">
-                <li><a href="{{$PREFIX}}/charts/budget/">เงินทุนการเลี้ยงโคเนื้อ</a></li>
+                <li><a href="{{url($PREFIX."/charts/budget/")}}">เงินทุนการเลี้ยงโคเนื้อ</a></li>
             </div>
         </div>
     </div>
@@ -155,38 +155,34 @@
         </div>
         <div id="collapse6" class="panel-collapse collapse in">
             <div class="panel-body">
-                <li><a href="{{$PREFIX}}/charts/normal/การจำหน่ายโค/seller_types">การจำหน่ายโคเนื้อให้กับใคร</a></li>
+                <li><a href="{{url($PREFIX."/charts/normal/การจำหน่ายโค/seller_types")}}">การจำหน่ายโคเนื้อให้กับใคร</a></li>
                 <li>
-                    <a href="{{$PREFIX}}/charts/normal/อายุของโคเนื้อที่จำหน่าย/age_sale">อายุของโคเนื้อที่จำหน่าย</a>
+                    <a href="{{url($PREFIX."/charts/normal/อายุของโคเนื้อที่จำหน่าย/age_sale")}}">อายุของโคเนื้อที่จำหน่าย</a>
                 </li>
 
                 <li>
-                    <a href="{{$PREFIX}}/charts/normal/น้ำหนักของโคเนื้อที่ท่านจำหน่าย/weight_range_sale">น้ำหนักของโคเนื้อที่ท่านจำหน่าย</a>
+                    <a href="{{url($PREFIX."/charts/normal/น้ำหนักของโคเนื้อที่ท่านจำหน่าย/weight_range_sale")}}">น้ำหนักของโคเนื้อที่ท่านจำหน่าย</a>
                 </li>
 
                 <li>
-                    <a href="{{$PREFIX}}/charts/pie/ลักษณะการจำหน่าย/cattle_sale_methods">ลักษณะการจำหน่ายโคเนื้อ</a>
+                    <a href="{{url($PREFIX."/charts/pie/ลักษณะการจำหน่าย/cattle_sale_methods")}}">ลักษณะการจำหน่ายโคเนื้อ</a>
                 </li>
-                <li><a href="{{$PREFIX}}/charts/groupJoin">การรวมกลุ่มวิสาหกิจชุมชนและสหกรณ์โคเนื้อ</a>
+                <li><a href="{{url($PREFIX."/charts/groupJoin")}}">การรวมกลุ่มวิสาหกิจชุมชนและสหกรณ์โคเนื้อ</a>
                 </li>
-                <li><a href="{{$PREFIX}}/charts/sold">ราคาของโคเนื้อที่ขาย และระยะเวลาในการเลี้ยง</a></li>
+                <li><a href="{{url($PREFIX."/charts/sold")}}">ราคาของโคเนื้อที่ขาย และระยะเวลาในการเลี้ยง</a></li>
                 <li>
-                    <a href="{{$PREFIX}}/charts/pie/ความประสงค์ในการรวมกลุ่มวิสาหกิจชุมชนและสหกรณ์โคเนื้อ/group_join_future">ความประสงค์ในการรวมกลุ่มวิสาหกิจชุมชนและสหกรณ์โคเนื้อ</a>
-                </li>
-                <li>
-                    <a href="{{$PREFIX}}/charts/normal/ความต้องการความช่วยเหลือจากสหกรณ์/cooperative_help_types">ความต้องการความช่วยเหลือจากสหกรณ์</a>
+                    <a href="{{url($PREFIX."/charts/pie/ความประสงค์ในการรวมกลุ่มวิสาหกิจชุมชนและสหกรณ์โคเนื้อ/group_join_future")}}">ความประสงค์ในการรวมกลุ่มวิสาหกิจชุมชนและสหกรณ์โคเนื้อ</a>
                 </li>
                 <li>
-                    <a href="{{$PREFIX}}/charts/normal/ความต้องการซื้ออาหารข้นในนามสหกรณ์/feed_purchase_cooperative">ความต้องการซื้ออาหารข้นในนามสหกรณ์</a>
+                    <a href="{{url($PREFIX."/charts/normal/ความต้องการความช่วยเหลือจากสหกรณ์/cooperative_help_types")}}">ความต้องการความช่วยเหลือจากสหกรณ์</a>
+                </li>
+                <li>
+                    <a href="{{url($PREFIX."/charts/normal/ความต้องการซื้ออาหารข้นในนามสหกรณ์/feed_purchase_cooperative")}}">ความต้องการซื้ออาหารข้นในนามสหกรณ์</a>
                 </li>
 
                 <li>
-                    <a href="{{$PREFIX}}/charts/normal/ระดับความพอใจต่อราคาที่ขาย/sale_satisfaction">ระดับความพอใจต่อราคาที่ขาย</a>
+                    <a href="{{url($PREFIX."/charts/normal/ระดับความพอใจต่อราคาที่ขาย/sale_satisfaction")}}">ระดับความพอใจต่อราคาที่ขาย</a>
                 </li>
-
-
-
-
             </div>
         </div>
     </div>
@@ -199,29 +195,29 @@
         </div>
         <div id="collapse7" class="panel-collapse collapse in">
             <div class="panel-body">
-                <li><a href="{{$PREFIX}}/charts/normal/หน่วยงานที่ให้บริการส่งเสริมการเลี้ยงโคเนื้อ/support_sources">หน่วยงานที่ให้บริการส่งเสริมการเลี้ยงโคเนื้อ</a>
+                <li><a href="{{url($PREFIX."/charts/normal/หน่วยงานที่ให้บริการส่งเสริมการเลี้ยงโคเนื้อ/support_sources")}}">หน่วยงานที่ให้บริการส่งเสริมการเลี้ยงโคเนื้อ</a>
                 </li>
                 <li>
-                    <a href="{{$PREFIX}}/charts/pie/การเยี่ยมเยือนฟาร์มจากหน่วยงาน/support_visit">การเยี่ยมเยือนฟาร์มจากหน่วยงาน</a>
-                </li>
-
-                <li>
-                    <a href="{{$PREFIX}}/charts/pie/การได้รับการสนับสนุนปัจจัยการผลิต/production_support">การได้รับการสนับสนุนปัจจัยการผลิต</a>
+                    <a href="{{url($PREFIX."/charts/pie/การเยี่ยมเยือนฟาร์มจากหน่วยงาน/support_visit")}}">การเยี่ยมเยือนฟาร์มจากหน่วยงาน</a>
                 </li>
 
                 <li>
-                    <a href="{{$PREFIX}}/charts/pie/การใช้บริการด้านสุขภาพโคจากหน่วยงานของกรมปศุสัตว์/cattle_heath_support">การใช้บริการด้านสุขภาพโคจากหน่วยงานของกรมปศุสัตว์</a>
-                </li>
-                <li>
-                    <a href="{{$PREFIX}}/charts/pie/การเข้ารับการฝึกอบรมเกี่ยวกับการเลี้ยงโคเนื้อ/training_support">การเข้ารับการฝึกอบรมเกี่ยวกับการเลี้ยงโคเนื้อ</a>
+                    <a href="{{url($PREFIX."/charts/pie/การได้รับการสนับสนุนปัจจัยการผลิต/production_support")}}">การได้รับการสนับสนุนปัจจัยการผลิต</a>
                 </li>
 
                 <li>
-                    <a href="{{$PREFIX}}/charts/pie/การไปดูงานเกี่ยวกับการเลี้ยงโคเนื้อ/observe_support">การไปดูงานเกี่ยวกับการเลี้ยงโคเนื้อ</a>
+                    <a href="{{url($PREFIX."/charts/pie/การใช้บริการด้านสุขภาพโคจากหน่วยงานของกรมปศุสัตว์/cattle_heath_support")}}">การใช้บริการด้านสุขภาพโคจากหน่วยงานของกรมปศุสัตว์</a>
+                </li>
+                <li>
+                    <a href="{{url($PREFIX."/charts/pie/การเข้ารับการฝึกอบรมเกี่ยวกับการเลี้ยงโคเนื้อ/training_support")}}">การเข้ารับการฝึกอบรมเกี่ยวกับการเลี้ยงโคเนื้อ</a>
                 </li>
 
                 <li>
-                    <a href="{{$PREFIX}}/charts/pie/การสนับสนุนให้แม่พันธุ์โคเนื้อมาเลี้ยงเพื่อผลิตลูก/female_breeder_support">การสนับสนุนให้แม่พันธุ์โคเนื้อมาเลี้ยงเพื่อผลิตลูก</a>
+                    <a href="{{url($PREFIX."/charts/pie/การไปดูงานเกี่ยวกับการเลี้ยงโคเนื้อ/observe_support")}}">การไปดูงานเกี่ยวกับการเลี้ยงโคเนื้อ</a>
+                </li>
+
+                <li>
+                    <a href="{{url($PREFIX."/charts/pie/การสนับสนุนให้แม่พันธุ์โคเนื้อมาเลี้ยงเพื่อผลิตลูก/female_breeder_support")}}">การสนับสนุนให้แม่พันธุ์โคเนื้อมาเลี้ยงเพื่อผลิตลูก</a>
                 </li>
             </div>
         </div>
@@ -235,7 +231,7 @@
         </div>
         <div id="collapse7" class="panel-collapse collapse in">
             <div class="panel-body">
-                <li><a href="{{$PREFIX}}/charts/suggestion">ตารางปัญหา อุปสรรคและข้อเสนอแนะ</a></li>
+                <li><a href="{{url($PREFIX."/charts/suggestion")}}">ตารางปัญหา อุปสรรคและข้อเสนอแนะ</a></li>
             </div>
         </div>
     </div>
